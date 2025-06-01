d930c8e72acd4afd40463da21ab2f981707b413a
Piotr Chodkowski <126429@uksw.edu.pl>
2025-06-01 12:24:59 +0200

    Naprawa formatu kodu 4

--- src/Insert.h
+++ src/Insert.h
@@ -14,19 +14,22 @@ inline SortError Insert<T, Container>::sort(SortData<T>& data)
 {
     SortError error = SE_SUCCESS;
 
-    if (data.begin(error) == data.end(error)) {
+    if (data.begin(error) == data.end(error))
+    {
         return SE_SUCCESS; // Empty container, nothing to sort
     }
 
     int n = data.size();
 
-    for (int i = 1; i < n; ++i) {
+    for (int i = 1; i < n; ++i)
+    {
         auto key = data[i];
         int j = i - 1;
 
         // Move elements of data[0..i-1] that are greater than key
         // to one position ahead of their current position
-        while (j >= 0 && data[j] > key) {
+        while (j >= 0 && data[j] > key)
+        {
             data[j + 1] = data[j];
             --j;
         }
